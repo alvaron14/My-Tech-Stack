2ef359aadceedbdc12efd32eaa28ced04fda5c3b
Alvaro G.C. <alvagomez@gmail.com>
2025-03-23 21:49:23 +0100

    feat: working prototype of OrderApi endpoint with mapping

--- api-rest/src/main/java/com/alvaro/infrastructure/rest/controller/OrderController.java
+++ api-rest/src/main/java/com/alvaro/infrastructure/rest/controller/OrderController.java
@@ -1,12 +1,23 @@
 package com.alvaro.infrastructure.rest.controller;
 
+import com.alvaro.domain.model.Order;
+import com.alvaro.domain.model.vo.OrderId;
+import com.alvaro.domain.projection.Pagination;
 import com.alvaro.infrastructure.rest.api.OrderApi;
 import com.alvaro.infrastructure.rest.api.dto.OrderSearchResultDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@CustomRestController
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
 public class OrderController implements OrderApi {
 
+    @Autowired
     private OrderApiMapper responseMapper;
 
     @GetMapping("/health")
@@ -17,6 +28,8 @@ public class OrderController implements OrderApi {
 
     @Override
     public OrderSearchResultDTO getOrders() {
-        return null;
+        return this.responseMapper.toResponse(
+            List.of(new Order(new OrderId(UUID.randomUUID()))),
+            new Pagination(10, 0L));
     }
 }
