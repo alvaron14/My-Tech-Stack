ace0afff7bb80a3f860f9ab8dd24e2a7d47be484
Álvaro Gómez Cuenca <alvagomez@gmail.com>
2025-06-15 20:47:25 +0200

    Cleanup controller (#27)
    * Cleanup controller
    
    * Clean imports

--- api-rest/src/main/java/com/alvaro/infrastructure/rest/controller/OrderController.java
+++ api-rest/src/main/java/com/alvaro/infrastructure/rest/controller/OrderController.java
@@ -10,7 +10,6 @@ import com.alvaro.framework.cqrs.core.query.QueryBus;
 import com.alvaro.infrastructure.rest.api.OrderApi;
 import com.alvaro.infrastructure.rest.api.dto.OrderSearchResultDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,11 +24,6 @@ public class OrderController implements OrderApi {
     private final QueryBus queryBus;
 
     private final OrderApiMapper responseMapper;
-
-    @GetMapping("/health")
-    public String getHealth() {
-        return "API is working";
-    }
 
     @Override
     public OrderSearchResultDTO getOrders() {
